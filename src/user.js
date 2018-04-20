0efea16128f8db5f1be6acdc7646df55a9813cff
Kristen Chellis <peanutster@gmail.com>
2018-04-20 11:50:22 -0700

    Add life Expectancy function that passes

--- src/user.js
+++ src/user.js
@@ -6,6 +6,7 @@ class User {
     this.state = state;
     this.today = new Date();
   }
+
   getSeconds() {
     let milliseconds =this.today.getTime() - this.birthday.getTime();
     let seconds = milliseconds / 1000
@@ -20,6 +21,21 @@ class User {
       planets.push(years/planetToEarth[i]);
     }
     return planets;
+  }
+  lifeExpectancy() {
+    let male = [72.6, 76.3, 77.2, 73.1, 78.3, 77.8, 78.6, 75.9, 76.6, 74.6, 78.0, 77.4, 76.4, 75.1, 77.3, 76.3, 73.4, 72.7, 76.8, 76.1, 78.1, 75.7, 78.7, 71.9, 74.9, 76.4, 77.7, 75.6, 78.0, 77.8, 75.6, 77.9, 75.1, 76.9, 75.2, 73.3, 77.2, 75.8, 77.2, 74.0, 76.6, 73.5, 76.0, 78.3, 78.2, 76.6, 77.8, 72.8, 77.5, 76.0];
+    let female = [78.2, 80.4, 82.1, 78.8, 83.1, 82.2, 82.9, 80.7, 82.3, 79.7, 84.7, 81.6, 81.4, 80.0, 82.0, 81.0, 78.5, 78.6, 81.5, 81.3, 82.7, 80.7, 83.3, 78.0, 80.1, 80.6, 81.9, 80.6, 82.5, 82.5, 81.3, 82.8, 80.4, 82.3, 80.2, 78.5, 81.8, 81.1, 82.3, 79.8, 82.4, 79.0, 80.8, 82.1, 82.6, 81.3, 82.0, 78.1, 82.4, 80.8];
+    let states = ["AL", "AK", "AZ", "AR", "CA", "CO", "CT", "DE", "FL", "GA", "HI", "ID", "IL", "IN", "IA", "KS", "KY", "LA", "ME", "MD", "MA", "MI", "MN", "MS", "MO", "MT", "NE", "NV", "NH", "NJ", "NM", "NY", "NC", "ND", "OH", "OK", "OR", "PA", "RI", "SC", "SD", "TN", "TX", "UT", "VT",  "VA", "WA", "WV", "WI", "WY"];
+    const secondsInYear = (365 * 24 * 60 * 60);
+    let years = this.getSeconds()/secondsInYear;
+    let index = states.indexOf(this.state);
+    let timeLeft;
+    if(this.sex === "male") {
+      timeLeft = male[index] - years;
+    }else {
+      timeLeft = female[index] - years;
+    }
+    return timeLeft;
   }
 }
 export { User };
