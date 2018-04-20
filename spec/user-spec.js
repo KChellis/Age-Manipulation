032d4e8158cba026703b4fde473385756c3c0cdf
Kristen Chellis <peanutster@gmail.com>
2018-04-20 10:16:36 -0700

    Add test for getSeconds function

--- spec/user-spec.js
+++ spec/user-spec.js
@@ -1,1 +1,13 @@
 import { User } from './../src/user.js';
+
+describe("User", function() {
+  let today = new Date();
+  let birthday = new Date(1985, 10, 17);
+  let user = new User("Kristen", birthday, "female", "OR");
+
+
+  it ("should calculate number of seconds a person has lived", function() {
+    let seconds = user.getSeconds();
+    expect(seconds).toBeCloseTo(1023177222, -6);
+  });
+});
